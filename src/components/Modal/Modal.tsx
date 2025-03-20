2d4416fc534f957299b11e62d494c462d892c7a0
Raman Novikau <demoikk@gmail.com>
2025-03-20 12:32:38 +0300

    feat: changed click outside logic to useClickOutside

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Styles from './Modal.module.css';
+import { useClickOutside } from '../../state/useClickOutside.ts';
 
 export type ModalProps = React.PropsWithChildren<{
   isOpen: boolean;
@@ -11,9 +12,17 @@ const Modal: React.FC<ModalProps> = (props) => {
   const { isOpen, title, onClose } = props;
   if (!isOpen) return null;
 
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  useClickOutside(ref, () => onClose());
+
   return (
-    <div className={Styles.modalOverlay} onClick={onClose}>
-      <div className={Styles.modalContent} onClick={(e) => e.stopPropagation()}>
+    <div className={Styles.modalOverlay} onClick={(e) => e.stopPropagation()}>
+      <div
+        ref={ref}
+        className={Styles.modalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className={Styles.modalHeader}>
           <h2>{title}</h2>
           <button className={Styles.closeBtn} onClick={onClose}>
